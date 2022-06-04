001acad3c4548bddc9490607acd6163c3f7a6433
Rasul Yunusov <rasul.yunusov282750@gmail.com>
2022-06-04 18:33:52 +0300

    update: by adding UUPS upgradeable fiture

--- ERC721Collection/contracts/LandNFT.sol
+++ ERC721Collection/contracts/LandNFT.sol
@@ -1,118 +1,119 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-pragma solidity >=0.7.0 <0.9.0;
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+uint256 constant _maxSupply = 1500000000; // upper bound
 
-uint256 constant maxSupply = 1500000000; // upper bound
+contract LandNFT is Initializable, ERC721Upgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    using CountersUpgradeable for CountersUpgradeable.Counter;
+    using Strings for uint256;
 
-contract LandNFT is ERC721, Ownable {
-  using Strings for uint256;
-  using Counters for Counters.Counter;
-
-  Counters.Counter private totalSupply;
-
-  string public uriSuffix = ".json";
-  string public baseURI = "";
+    CountersUpgradeable.Counter private _tokenIdCounter;
+    string public _uriSuffix = ".json";
+    string public _baseURI = "https://gateway.pinata.cloud/ipfs/QmNd5MKmkM4VhXgo9J9DvGEAM4XWZwyvBVtZ1K6iAjEkhr/";
   
-  uint256 public maxMintAmountPerTx = 10000;
-  bool public paused = true;
+    uint256 public _maxMintAmountPerTx = 10000;
 
-  constructor() ERC721("LandNFT", "NFL") {
-    setBaseURI("https://gateway.pinata.cloud/ipfs/QmNd5MKmkM4VhXgo9J9DvGEAM4XWZwyvBVtZ1K6iAjEkhr/");
-  }
 
-  modifier mintCompliance(uint256 _mintAmount) {
-    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, "Invalid mint amount!");
-    require(supply.current() + _mintAmount <= maxSupply, "Max supply exceeded!");
-    _;
-  }
-
-  function totalSupply() public view returns (uint256) {
-    return supply.current();
-  }
-
-  function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) {
-    require(!paused, "The contract is paused!");
-
-    _mintLoop(msg.sender, _mintAmount);
-  }
-  
-  function mintForAddress(uint256 _mintAmount, address _receiver) public mintCompliance(_mintAmount) onlyOwner {
-    _mintLoop(_receiver, _mintAmount);
-  }
-
-  function walletOfOwner(address _owner)
-    public
-    view
-    returns (uint256[] memory)
-  {
-    uint256 ownerTokenCount = balanceOf(_owner);
-    uint256[] memory ownedTokenIds = new uint256[](ownerTokenCount);
-    uint256 currentTokenId = 1;
-    uint256 ownedTokenIndex = 0;
-
-    while (ownedTokenIndex < ownerTokenCount && currentTokenId <= maxSupply) {
-      address currentTokenOwner = ownerOf(currentTokenId);
-
-      if (currentTokenOwner == _owner) {
-        ownedTokenIds[ownedTokenIndex] = currentTokenId;
-
-        ownedTokenIndex++;
-      }
-
-      currentTokenId++;
+    constructor() {
+        _disableInitializers();
     }
 
-    return ownedTokenIds;
-  }
 
-  function tokenURI(uint256 _tokenId)
-    public
-    view
-    virtual
-    override
-    returns (string memory)
-  {
-    require(
-      _exists(_tokenId),
-      "ERC721Metadata: URI query for nonexistent token"
-    );
-
-    string memory currentBaseURI = _baseURI();
-    return bytes(currentBaseURI).length > 0
-        ? string(abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix))
-        : "";
-  }
-
-  function setMaxMintAmountPerTx(uint256 _maxMintAmountPerTx) public onlyOwner {
-    maxMintAmountPerTx = _maxMintAmountPerTx;
-  }
-
-  function setBaseURI(string memory _newBaseURI) public onlyOwner {
-    baseURI = _newBaseURI;
-  }
-
-
-  function setUriSuffix(string memory _uriSuffix) public onlyOwner {
-    uriSuffix = _uriSuffix;
-  }
-
-  function setPaused(bool _state) public onlyOwner {
-    paused = _state;
-  }
-
-  function _mintLoop(address _receiver, uint256 _mintAmount) internal {
-    for (uint256 i = 0; i < _mintAmount; i++) {
-      supply.increment();
-      _safeMint(_receiver, supply.current());
+    function initialize() initializer public {
+        __ERC721_init("LandNFT", "NFL");
+        __Ownable_init();
+        __UUPSUpgradeable_init();
     }
-  }
 
-  function _baseURI() internal view virtual override returns (string memory) {
-    return baseURI;
-  }
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+
+
+    modifier mintCompliance(uint256 mintAmount) {
+        require(mintAmount > 0 && mintAmount <= _maxMintAmountPerTx, "Invalid mint amount!");
+        require(_tokenIdCounter.current() + mintAmount <= _maxSupply, "Max supply exceeded!");
+        _;
+    }
+
+    function mintForAddress(uint256 mintAmount, address receiver) public mintCompliance(mintAmount) onlyOwner {
+        _mintLoop(receiver, mintAmount);
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIdCounter.current();
+    }
+
+    function walletOfOwner(address _owner)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory ownedTokenIds = new uint256[](ownerTokenCount);
+        uint256 currentTokenId = 1;
+        uint256 ownedTokenIndex = 0;
+
+        while (ownedTokenIndex < ownerTokenCount && currentTokenId <= maxSupply) {
+        address currentTokenOwner = ownerOf(currentTokenId);
+
+            if (currentTokenOwner == _owner) {
+                ownedTokenIds[ownedTokenIndex] = currentTokenId;
+                ownedTokenIndex++;
+            }
+
+            currentTokenId++;
+        }
+
+        return ownedTokenIds;
+    }
+
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        
+        require(_exists(_tokenId),"ERC721Metadata: URI query for nonexistent token");
+
+        string memory currentBaseURI = _baseURI();
+        return bytes(currentBaseURI).length > 0
+            ? string(abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix))
+            : "";
+    }
+
+    function setMaxMintAmountPerTx(uint256 _maxMintAmountPerTx) public onlyOwner {
+        maxMintAmountPerTx = _maxMintAmountPerTx;
+    }
+
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+    function setUriSuffix(string memory _uriSuffix) public onlyOwner {
+        uriSuffix = _uriSuffix;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function _mintLoop(address receiver, uint256 mintAmount) internal {
+        for (uint256 i = 0; i < mintAmount; i++) {
+         _tokenIdCounter.increment();
+        _safeMint(receiver, _tokenIdCounter.current());
+        }
+    }
+
 }
 
