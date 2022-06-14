1e7c9f2bda149fb46957e5856c72c9707d440993
Rasul <rasul.yunusov282750@gmail.com>
2022-06-14 20:30:46 +0300

    update: the deployment script to use UUPS instead of transparent proxy

--- NFTCollectionGreensFI/scripts/sample-script.js
+++ NFTCollectionGreensFI/scripts/sample-script.js
@@ -3,7 +3,7 @@ const {ethers, upgrades} = require("hardhat");
 async function main() {
 
     const LandNFT = await ethers.getContractFactory("LandNFT");
-    const landNFT = await upgrades.deployProxy(LandNFT, {initializer: "initialize"});
+    const landNFT = await upgrades.deployProxy(LandNFT, {kind: 'uups'});
 
     await landNFT.deployed();
 
